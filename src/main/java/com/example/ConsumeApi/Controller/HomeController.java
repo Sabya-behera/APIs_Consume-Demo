ac07522488e91c0bab1f67d95824c4f5cf772bc9
Sabya-behera <sabyasacheebeherappl@gmail.com>
2022-01-06 13:25:35 +0530

    files added

--- src/main/java/com/example/ConsumeApi/Controller/HomeController.java
+++ src/main/java/com/example/ConsumeApi/Controller/HomeController.java
@@ -2,12 +2,15 @@ package com.example.ConsumeApi.Controller;
 
 import com.example.ConsumeApi.Model.User;
 import com.example.ConsumeApi.Service.ServiceLayer;
-import org.apache.tomcat.util.http.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 public class HomeController {
@@ -18,19 +21,57 @@ public class HomeController {
         this.serviceLayer = serviceLayer;
     }
 
-    @GetMapping("/coin")
-    public ResponseEntity<User> getData(){
-     return new ResponseEntity<>(serviceLayer.consumeAPI(), HttpStatus.OK);}
+    @GetMapping("/user")
+    public ResponseEntity<Object> getData(){
+       // System.out.println("THE VALUES "+serviceLayer.consumeAPI().toString());
+     return new ResponseEntity<>(serviceLayer.consumeAPI(), HttpStatus.OK);
+    }
 
 //    @GetMapping(value = "api/v1/exchange/currencies")
 //    public ResponseDTO<?> getCurrencies(@RequestHeader(name = "Accept-Language", required = false) Locale locale) throws JsonProcessingException {
 //        return responseUtil.ok(exchangeCoinService.getCurrencies(),"success.text");
 //    }
+    @PostMapping(value = "/users/{id}")
+    public ResponseEntity<User> getUser(/*@ModelAttribute("user")*/ @RequestBody User user , UriComponentsBuilder builder) throws URISyntaxException {
+         serviceLayer.getUser(user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+        return new ResponseEntity<User>(user, headers, HttpStatus.CREATED);
+    }
 
     @GetMapping("/group")
     public String group()
     {
         return "Group";
     }
-//    ResponseUtil
+
+
+    @GetMapping("user/{id}")
+    public User user(@PathVariable("id") String id){
+       // logger.info("calling userList() to get list of users");
+        return serviceLayer.getUser(id);
+    }
+
+    @GetMapping("userList")
+    public List<User> userList(){
+       // logger.info("calling userList() to get list of users");
+        return serviceLayer.getUserList();
+    }
+
+    @PostMapping("user")
+    public String addUser(@RequestBody User user){
+        //logger.info("adding user");
+        return serviceLayer.addUser(user);
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable String id){
+       // logger.info("deleting user - {}", id);
+        return serviceLayer.deleteUser(id);
+    }
+
+    @PutMapping("/updateAddress/{id}/{newAddress}")
+    String updateAddress(@PathVariable String id, @PathVariable String newAddress){
+       // logger.info("updating user - {}", id);
+        return serviceLayer.updateAddress(id, newAddress);
+    }
 }
--- src/main/java/com/example/ConsumeApi/Service/ServiceLayer.java
+++ src/main/java/com/example/ConsumeApi/Service/ServiceLayer.java
@@ -1,21 +1,81 @@
 package com.example.ConsumeApi.Service;
 
 import com.example.ConsumeApi.Model.User;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ServiceLayer {
     private final RestTemplate restTemplate;
 
     @Autowired
-    public ServiceLayer(RestTemplate restTemplate)
-    {
-        this.restTemplate=restTemplate;
+    public ServiceLayer(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
-    public User consumeAPI()
-    {
-        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/todos/1",User.class);
+
+    public List<User> consumeAPI() {
+        return (List<User>) restTemplate.getForObject("https://jsonplaceholder.typicode.com/todos", Object.class);
+    }
+
+    @JsonAnyGetter
+    public void getUser(User user) throws URISyntaxException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        URI uri = new URI("https://jsonplaceholder.typicode.com/todos");
+        user.setUserId(11);
+        user.setId(1);
+        user.setTitle("delectus aut autem");
+        HttpEntity<User> httpEntity = new HttpEntity<>(user, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        User user2 = restTemplate.postForObject(uri, httpEntity, User.class);
+        System.out.println("Id: " + user2.getId());
+
+//        Map user= new HashMap();
+//        user.put(user.get(userId),userId);
+//        user.put(user.get(id),id);
+//        user.put(user.get(title),title);
+//        HttpEntity<Map<Object, List<Object>>> httpEntity = new HttpEntity<Map<Object,List<Object>>>(user, headers);
+//        return (Map<String, Object>) restTemplate.postForObject("https://jsonplaceholder.typicode.com/todos",httpEntity,Object.class);
+
+    }
+
+    //    @JsonAnySetter
+//    public void setUser(User u, User user) {
+//        // IMPORTANT
+//        // Here you have to create or find appropriate Resource in your code
+//        // and add current task to it
+//        System.out.println(u+" "+ user);
+//    }
+    @FeignClient(name = "${service.name}", url = "${service.base.url}")
+    public interface UserService {
+
+        @GetMapping("/user/{id}")
+        User getUser(@PathVariable("id") String id)
+        @GetMapping(value = "/users")
+        List<User> getUserList()
+        @PostMapping("/user")
+        String addUser(User user)
+
+        @DeleteMapping("/user/{id}")
+        String deleteUser(@PathVariable("id") String id)
+
+        @PutMapping("/updateAddress/{id}/{newAddress}")
+        String updateAddress(@PathVariable("id") String id, @PathVariable("newAddress") String newAddress)
+
     }
 }
